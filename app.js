f2f7cc987b46b93d92b7d1ab4b574d9af1e5c49c
Djibril Nkeubou <djibrilnkeubou@gmail.com>
2026-04-13 13:08:19 -0400

    update version

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Version 2  CI/CD fonctionne ! ");
+  res.send("Version 3  CI/CD fonctionne ! ");
 });
 
 app.listen(3000, () => console.log("Running on port 3000 - app.js:8"));
